4691ba7642016bc579bbdc1e674b63ee0125c206
Brian Casey <b.casey3@nuigalway.ie>
2017-09-23 21:40:24 +0100

    Updated code

--- src/main/java/com/mycompany/mavendriver/Main.java
+++ src/main/java/com/mycompany/mavendriver/Main.java
@@ -5,6 +5,10 @@
  */
 package com.mycompany.mavendriver;
 
+import com.mycompany.CourseProgramme;
+import com.mycompany.Module;
+import com.mycompany.Student;
+
 /**
  *
  * @author I330151
@@ -13,6 +17,18 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
+        Student s1 = new Student("Aria", 21, "25/04/1996");
+        System.out.println(s1.getUsername());
+        
+        Student s2 = new Student("Anna", 21, "20/04/1996");
+        Student s3 = new Student("Annabelle", 2, "20/07/2015");
+        
+        Module m1 = new Module("Software Engineering |||", "CT417");
+        m1.addStudent(s1);
+        Module m2 = new Module("Artificial Intelligence", "CT421");
+        Module m3 = new Module("Cryptography", "CS402");
+        
+        CourseProgramme cp1 = new CourseProgramme("Computer Science and Information Technology");
+        cp1.addModule(m1);
     }
-    
 }

897398baf5d479963123de07dc773bd72ffc4852
Brian Casey <b.casey3@nuigalway.ie>
2017-10-05 17:47:05 +0100

    Prints out details

--- src/main/java/com/mycompany/mavendriver/Main.java
+++ src/main/java/com/mycompany/mavendriver/Main.java
@@ -14,8 +14,7 @@ import org.joda.time.DateTime;
 public class Main {
     
     public static void main(String[] args) {
-        
-        
+       
         System.out.println("Hello World!"); // Display the string.
         Student s1 = new Student("Aria", 21, "25/04/1996");
         System.out.println(s1.getUsername());
@@ -23,31 +22,29 @@ public class Main {
         Student s2 = new Student("Anna", 21, "20/04/1996");
         Student s3 = new Student("Annabelle", 2, "20/07/2015");
         
-        Module m1 = new Module("Software Engineering |||", "CT417");
-        Module m2 = new Module("Artificial Intelligence", "CT421");
-        Module m3 = new Module("Cryptography", "CS402");
+        Module CT417 = new Module("Software Engineering |||", "CT417");
+        Module CT421 = new Module("Artificial Intelligence", "CT421");
+        Module CS402 = new Module("Cryptography", "CS402");
         
-        m1.addStudent(s1);
-        m2.addStudent(s1);
-        m3.addStudent(s1);
+        CT417.addStudent(s1);
+        CT417.addStudent(s2);
+        CT417.addStudent(s3);
 
-        m1.addStudent(s2);
-        m2.addStudent(s2);
-        m3.addStudent(s2);
+        CT421.addStudent(s1);
+        CT421.addStudent(s2);
+        CT421.addStudent(s3);
         
-        m1.addStudent(s3);
-        m2.addStudent(s3);
-        m3.addStudent(s3);
+        CS402.addStudent(s1);
+        CS402.addStudent(s2);
+        CS402.addStudent(s3);
         
-        System.out.println(m1.getStudents());
+        System.out.println(CT417.getStudents());
         
-        
-        CourseProgramme cp1;
-        cp1 = new CourseProgramme("Computer Science and Information Technology", new DateTime(1-8-17), new DateTime(1-8-18));
-        cp1.addModule(m1);
-        cp1.addModule(m2);
-        cp1.addModule(m3);
-        System.out.println(cp1.getModules());
+        CourseProgramme CSIT = new CourseProgramme("Computer Science and Information Technology", new DateTime(1-8-17), new DateTime(1-8-18));
+        CSIT.addModule(CT417);
+        CSIT.addModule(CT421);
+        CSIT.addModule(CS402);
+        System.out.println(CSIT.getModules());
         
     }
 }
